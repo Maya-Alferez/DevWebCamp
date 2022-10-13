e4bf7c9bacfc998b8d77e80b13bc5d9e73b6ac5d
Maya-Alferez <m.alferez17@gmail.com>
2022-10-13 18:02:02 -0500

    Autenticación, Registro
    Se crean las páginas de iniciar sesión, registro. También se le agregan las vistas.

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\AuthController;
 
 $router = new Router();
 
---
+++ views/auth/login.php
@@ -0,0 +1,23 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Inicia sesión en DevWebCamp</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input type="email" class="formulario__input" placeholder="Email" id="email" name="email">
+        </div>
+
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Contraseña</label>
+            <input type="password" class="formulario__input" placeholder="Contraseña" id="password" name="password">
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Iniciar sesión">
+    </form>
+
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aún no tienes una cuenta?, Regístrate</a>
+        <a href="/olvide" class="acciones__enlace">¿Olvidaste tú password?</a>
+    </div>
+</main>
