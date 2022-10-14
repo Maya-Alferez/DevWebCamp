fc917d8d2663890f09a7b9c7aa06823c7cd1143b
Maya-Alferez <m.alferez17@gmail.com>
2022-10-14 15:00:48 -0500

    Header
    Se crea el header y se le agrega diseño, tanto para tamaño desktop, así como móvil.

---
+++ views/auth/olvide.php
@@ -0,0 +1,18 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Recupera tu acceso a DevWebCamp</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input type="email" class="formulario__input" placeholder="Email" id="email" name="email">
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Enviar instrucciones">
+    </form>
+
+    <div class="acciones">
+        <a href="/login" class="acciones__enlace">¿Ya tienes cuenta? Inicia sesión</a>
+        <a href="/registro" class="acciones__enlace">¿Aún no tienes una cuenta?, Regístrate</a>
+    </div>
+</main>
